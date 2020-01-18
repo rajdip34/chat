8a6d6df668915da7d208db8a4e5631f4e7692de3
rajdip34 <rajdipmondal34@gmail.com>
2020-01-18 22:23:57 +0530

    first update

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+// import 'package:flutter/material.dart';
+// final themeColor = Color;
+final themeColor = Color(0xff2f18b5);
+final primaryColor = Color(0xff203152);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
